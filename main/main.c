50601bab582910c4e8972b582d173f90247238d0
uni369 <unicabpp@yahoo.com>
2025-12-28 13:30:19 -0500

    WORKING log file.

--- main/main.c
+++ main/main.c
@@ -19,13 +19,13 @@ static const char *TAG = "[ESP-MESS]";
 
 void app_main(void) {
 	//! nvs_flash required for WiFi, ESP-NOW, and other stuff.
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK(ret);
-    ESP_LOGI(TAG, "APP START");
+	esp_err_t ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
+	ESP_LOGI(TAG, "APP START");
 
 	//# Setup Blinking
 	static uint8_t s_led_state = 0;
@@ -35,41 +35,45 @@ void app_main(void) {
 	//# Setup Wifi
 	wifi_init_sta(EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASSWORD);
 
-    //# Init SPI1 peripherals
-    M_Spi_Conf spi_conf0 = {
-        .host = 1,
-        .mosi = CONFIG_SPI_MOSI,
-        .miso = CONFIG_SPI_MISO,
-        .clk = CONFIG_SPI_CLK,
-        .cs = -1,
-    };
+	//# Init SPI1 peripherals
+	M_Spi_Conf spi_conf0 = {
+		.host = 1,
+		.mosi = CONFIG_SPI_MOSI,
+		.miso = CONFIG_SPI_MISO,
+		.clk = CONFIG_SPI_CLK,
+		.cs = -1,
+	};
 
-    ret = mod_spi_init(&spi_conf0, 20E6);
+	ret = mod_spi_init(&spi_conf0, 20E6);
 
-    if (ret == ESP_OK) {
-        //! NOTE: for MMC D3 or CS needs to be pullup if not used otherwise it will go into SPI mode
-        sd_spi_config(spi_conf0.host, spi_conf0.cs);
-        sd_test();
-    }
+	if (ret == ESP_OK) {
+		//! NOTE: for MMC D3 or CS needs to be pullup if not used otherwise it will go into SPI mode
+		sd_spi_config(spi_conf0.host, spi_conf0.cs);
+		sd_test();
+	}
 
-    int counter = 0;
+	int counter = 0;
 
 	while (1) {
 		// ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
 		gpio_set_level(BLINK_GPIO, s_led_state);
-        s_led_state = !s_led_state;
+		s_led_state = !s_led_state;
 
-        struct tm timeinfo = timeinfo_now();
-        if (timeinfo.tm_year > 70) {
-            // year number starts at 1900, epoch year is 1970
-            ESP_LOGI(TAG_WIFI, "Time: %s", GET_TIME_STR);
-        }
+		struct tm timeinfo = timeinfo_now();
+		if (timeinfo.tm_year > 70) {
+			// year number starts at 1900, epoch year is 1970
+			ESP_LOGI(TAG_WIFI, "Time: %s", GET_TIME_STR);
+			
+			char output[32];
+			sprintf(output, "%d,3,4,5,6\n", counter++);
+			sd_log_data("aabbcc", "20251228", output);
+		}
 
-        // char output[32];
-        // sprintf(output, "%d,3,4,5,6\n", counter++);
-        // sd_write_data(MOUNT_POINT"/test.csv", output);
-        
-        wifi_poll();
+		// char output[32];
+		// sprintf(output, "%d,3,4,5,6\n", counter++);
+		// sd_write_data(MOUNT_POINT"/test.csv", output);
+		
+		wifi_poll();
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
 }
